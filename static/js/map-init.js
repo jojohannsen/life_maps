912eea993dd0ef1545a6acd4b01eb5ebc5634387
Joh Johannsen <johannes.johannsen@gmail.com>
2025-02-06 10:22:24 -0800

    somewhat reasonable

--- static/js/map-init.js
+++ static/js/map-init.js
@@ -28,13 +28,25 @@ function alpha_only(str) {
     return str.replace(/[^a-zA-Z]/g, '');
 }
 
+// list of transparent colors
+const transparent_colors = ['#ff000080', '#00ff0080', '#0000ff80', '#ffff0080', '#ff00ff80', '#00ffff80', '#ffffff80', '#00000080'];
+// dictionary mapping person to color
+const person_colors = {};
+// next color to assign
+let next_color = 0;
+
 function add_person_location(person, map, lat, lon, years) {
     console.log("Adding person marker", lat, lon, years)
     const el = document.createElement('div');
     const width = 14 + years * 2;
     const height = 14 + years * 2;
+    // get color for person, or assign a new one if they don't have one
+    if (!person_colors[person]) {
+        person_colors[person] = transparent_colors[next_color];
+        next_color = (next_color + 1) % transparent_colors.length;
+    }   
     el.className = 'mkr_' + alpha_only(person);
-    el.style.backgroundColor = '#ff000080';
+    el.style.backgroundColor = person_colors[person];
     el.style.width = `${width}px`;
     el.style.height = `${height}px`;
     el.style.backgroundSize = '100%';
