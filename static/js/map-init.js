8d2508ebb8b43f4b8e80d9b9c94008c6a5835791
Joh Johannsen <johannes.johannsen@gmail.com>
2025-02-06 17:15:37 -0800

    concentric circles

--- static/js/map-init.js
+++ static/js/map-init.js
@@ -29,36 +29,52 @@ function alpha_only(str) {
 }
 
 // list of transparent colors
-const transparent_colors = ['#ff000080', '#00ff0080', '#0000ff80', '#ffff0080', '#ff00ff80', '#00ffff80', '#ffffff80', '#00000080'];
+const transparent_colors = ['#ff000080', '#0000ff80', '#ff00ff80', '#00ffff80', '#ffffff80', '#00000080'];
 // dictionary mapping person to color
 const person_colors = {};
 // next color to assign
 let next_color = 0;
+// non transparent colors
+const non_transparent_colors = ['#ff0000', '#0000ff', '#ff00ff', '#00ffff', '#ffffff', '#000000'];
+// dictionary mapping person to non transparent color
+const person_non_transparent_colors = {};
+// next non transparent color to assign
+let next_non_transparent_color = 0;
 
 function add_person_location(person, map, lat, lon, years) {
     console.log("Adding person marker", lat, lon, years)
-    const el = document.createElement('div');
-    const width = 14 + years * 2;
-    const height = 14 + years * 2;
+
+
     // get color for person, or assign a new one if they don't have one
     if (!person_colors[person]) {
         person_colors[person] = transparent_colors[next_color];
         next_color = (next_color + 1) % transparent_colors.length;
     }   
-    el.className = 'mkr_' + alpha_only(person);
-    el.style.backgroundColor = person_colors[person];
-    el.style.width = `${width}px`;
-    el.style.height = `${height}px`;
-    el.style.backgroundSize = '100%';
-    el.style.borderRadius = '50%';
+    if (!person_non_transparent_colors[person]) {
+        person_non_transparent_colors[person] = non_transparent_colors[next_non_transparent_color];
+        next_non_transparent_color = (next_non_transparent_color + 1) % non_transparent_colors.length;
+    }
+    // make concentric circles from 1 to years
+    for (let i = 0; i < years; i++) {
+        const width = 8 + i * 6;
+        const height = 8 + i * 6;
+        const el = document.createElement('div');
+        el.className = 'mkr_' + alpha_only(person);
+        el.style.width = `${width}px`;
+        el.style.height = `${height}px`;
+        el.style.backgroundSize = '100%';
+        el.style.borderRadius = '50%';
 
-    el.addEventListener('click', () => {
-        window.alert(marker.properties.message);
-    });
+        // first circle is solid, no border, the rest are transparent with border
+        el.style.background = i === 0 ? person_non_transparent_colors[person] : 'transparent';
+        el.style.border = i === 0 ? 'none' : `1px solid ${person_non_transparent_colors[person]}`;
+        el.addEventListener('click', () => {
+            window.alert(marker.properties.message);
+        });
 
-    // Add markers to the map.
-    new mapboxgl.Marker(el)
-        .setLngLat([lon, lat])
-        .addTo(map);
-//    return marker;
+        // Add markers to the map.
+        new mapboxgl.Marker(el)
+            .setLngLat([lon, lat])
+            .addTo(map);
+    }
 }
